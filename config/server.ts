af2c66ccaa7e710d47f76e5cc7968b6e4c44599f
eraldvelcani <eraldvelcani2003@gmail.com>
2025-09-28 19:13:21 +0200

    fourth

--- config/server.ts
+++ config/server.ts
@@ -6,14 +6,31 @@
 //     keys: env.array('APP_KEYS'),
 //   },
 // });
+
+
+// module.exports = ({ env }) => ({
+//   url: env('PUBLIC_URL', 'https://cookwithme-backend.onrender.com'),
+//   app: { keys: env.array('APP_KEYS', ['yourKey1', 'yourKey2']) },
+//   proxy: true, // 👈 Trust Render’s proxy so HTTPS is respected
+//   admin: {
+//     auth: {
+//       secret: env('ADMIN_JWT_SECRET'),
+//     },
+//     // You usually don’t need to force cookies here
+//   },
+// });
+
 module.exports = ({ env }) => ({
+  host: '0.0.0.0',
+  port: env.int('PORT', 10000),
   url: env('PUBLIC_URL', 'https://cookwithme-backend.onrender.com'),
-  app: { keys: env.array('APP_KEYS', ['yourKey1', 'yourKey2']) },
-  proxy: true, // 👈 Trust Render’s proxy so HTTPS is respected
+  proxy: true, // 👈 trust Render's proxy so HTTPS is respected
+  app: {
+    keys: env.array('APP_KEYS', ['defaultKey1', 'defaultKey2']),
+  },
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+      secret: env('ADMIN_JWT_SECRET', 'someRandomSecret'),
     },
-    // You usually don’t need to force cookies here
   },
 });
